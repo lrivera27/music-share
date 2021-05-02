fbd2a4b7e62b32d357aa5a9fb8d18faa34fbba8d
Luis Rivera <lriveraf.27@gmail.com>
2021-05-02 08:47:28 -0400

    Added react router

---
+++ app/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Home from "./pages/home";
+
+const App = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
+  );
+};
+
+export default App;
